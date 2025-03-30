050c33b2ad9093634edce2872d5a3361952a28c1
Ignas Panavas <76080358+IgnasPanavas@users.noreply.github.com>
2025-03-30 13:36:09 -0400

    wave field update

--- ignas-panavas-site/src/app/about/page.tsx
+++ ignas-panavas-site/src/app/about/page.tsx
@@ -4,25 +4,7 @@ export default function About() {
     return (
       <div>
         <main className='flex justify-center items-center min-h-screen'>
-            <div className="flex flex-col justify-center">
-            <div className="flex flex-1 flex-col items-center py-24 space-y-5 ">
-                <h1 className="text-5xl">About Me</h1>
-                <p className="text-center max-w-2xl">
-                I&apos;m Ignas Panavas, a Computer Science and Finance student at the University of Georgia. 
-                Passionate about building innovative software solutions, I enjoy blending my technical skills with financial insights.
-                When I&apos;m not coding or analyzing markets, you can find me running with NEST, sipping iced coffee, or exploring antique stores.
-                </p>
-                <p className="text-center max-w-2xl">
-                Whether it&apos;s crafting full-stack applications, designing trading journals, or leading projects, I strive to deliver impactful results. 
-                Let&apos;s create something amazing together!
-                </p>
-            </div>
-                <div className="flex flex-1">
-                        <Gallery/>
-                    
-                </div>
             
-          </div>
         </main>
       </div>
     );
--- ignas-panavas-site/src/components/NavBar.tsx
+++ ignas-panavas-site/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import Link from 'next/link';
 import { useState } from 'react';
@@ -11,24 +11,21 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="p-4 top-0 z-50">
+    <nav className="fixed top-0 left-0 w-full p-4 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className=" text-lg font-bold">
+        <div className="text-lg text-white">
           <Link href="/">Ignas Panavas</Link>
         </div>
         <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="focus:outline-none"
-          >
+          <button onClick={toggleMenu} className="focus:outline-none">
             ☰
           </button>
         </div>
         <div className={`md:flex space-x-20 ${isOpen ? 'block' : 'hidden'} md:block`}>
-          <Link href="/" className=" hover:text-gray-400">Home</Link>
-          <Link href="/about" className=" hover:text-gray-400">About</Link>
-          <Link href="/projects" className=" hover:text-gray-400">Projects</Link>
-          <Link href="/resume" className=" hover:text-gray-400">Resume</Link>
+          <Link href="/" className=" hover:underline text-white">Home</Link>
+          <Link href="/about" className="hover:underline text-white">About</Link>
+          <Link href="/projects" className="hover:underline text-white">Projects</Link>
+          <Link href="/resume" className="hover:underline text-white">Resume</Link>
         </div>
       </div>
     </nav>
