9ae99461fe32ce3ba1bfdf796d185fa0e9f4fb7a
Ignas Panavas <76080358+IgnasPanavas@users.noreply.github.com>
2025-09-10 16:01:51 -0400

    minor fix

--- ignas-panavas-site/src/app/fonts.tsx
+++ ignas-panavas-site/src/app/fonts.tsx
@@ -5,6 +5,6 @@ export const tinos = localFont({
       { path: './fonts/Tinos-Regular.ttf', weight: '400', style: 'normal' },
       { path: './fonts/Tinos-Italic.ttf', weight: '400', style: 'italic' },
       { path: './fonts/Tinos-Bold.ttf', weight: '700', style: 'normal' },
-      { path: './fonts/Tinos-Bolditalic.ttf', weight: '700', style: 'italic' },
+      { path: './fonts/Tinos-BoldItalic.ttf', weight: '700', style: 'italic' },
     ],
   });
